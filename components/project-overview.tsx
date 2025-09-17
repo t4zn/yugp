37ee047295cbcd08cbeb093ed6c7bc918a43679d
Taizun <165426422+t4zn@users.noreply.github.com>
2025-09-17 23:35:36 +0530

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -4,26 +4,8 @@ export const ProjectOverview = () => {
     <div className="flex flex-col items-center justify-end">
       <h1 className="text-3xl font-semibold mb-4">Taizun AI</h1>
       <p className="text-center">
-        Advanced AI assistant powered by Taizun 
+        Advanced AI assistant powered by Taizun
       </p>
     </div>
-  );
-};
-
-const Link = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <NextLink
-      target="_blank"
-      className="text-blue-500 hover:text-blue-600 transition-colors duration-75"
-      href={href}
-    >
-      {children}
-    </NextLink>
   );
 };
