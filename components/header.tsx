8db62cc33bb16b20fab9f4a1d815ba08f4264a34
Taizun <165426422+t4zn@users.noreply.github.com>
2025-09-18 00:17:36 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const Header: FC = () => {
   return (
     <div className="fixed right-0 left-0 w-full top-0 bg-white dark:bg-zinc-950 p-4">
-      <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-100">AI Chat</h1>
+      <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-100">Taizun AI</h1>
     </div>
   );
 };
