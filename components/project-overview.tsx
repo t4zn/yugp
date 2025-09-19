b5c331a0122a66dae9c1e64b0fcab18365c44022
Taizun <165426422+t4zn@users.noreply.github.com>
2025-09-19 07:19:21 +0530

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,6 +1,6 @@
 export const ProjectOverview = () => {
   return (
-    <div className="flex flex-col items-start pt-4 md:pt-28 pb-8">
+    <div className="flex flex-col items-start pt-12 md:pt-28 pb-8">
       <h1 className="text-xl sm:text-2xl font-semibold tracking-tight text-black drop-shadow-lg">Hello there!</h1>
       <p className="mt-3 text-lg text-gray-600 drop-shadow-lg">How can I help you today?</p>
     </div>
